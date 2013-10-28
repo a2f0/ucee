40c407e42c54af175d85df251146a44d869e0714
malvisio <alvisio@uchicago.edu>
2013-10-27 22:51:35 -0500

    changes to matcheng

--- matcheng.cpp
+++ matcheng.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "messages.h"
+#include <cstdlib>
+
+int higher(Order orderA, Order orderB, enum SIDE bos){
+  if(atof(orderA.price) > atof(orderB.price)){
+    return (bos==BUY)? 1:0;
+  }else if(atof(orderA.price) < atof(orderB.price)){
+    return (bos==SELL)? 1:0;
+  }else{
+    return (orderA.timestamp < orderB.timestamp)? 1:0;
+  };
+};
 
 int main(){
-    printf("starting matching engine\n"); 
-    return 0;
-}
+  Order orderA = {LIMIT_ORDER,"5647","Charlie","4X54",21345,BUY,"MSFT","11.3",500};
+  Order orderB = {LIMIT_ORDER,"5314","Delta","450X",24520,BUY,"MSFT","11.4",540};
+  printf("%d\n", higher(orderA,orderB,BUY));
+  printf("starting matching engine\n");
+  return 0;
+};
