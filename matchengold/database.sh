07a4481d62dcdbff059c193976c1a52d0c07749b
Marcelo Julio Alvisio <alvisio@linux2.cs.uchicago.edu>
2013-11-12 20:54:40 -0600

    removed unnecessary files

--- matchengold/database.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sqlite3 OrderBook.db "create table t1 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
