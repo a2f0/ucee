0181d558e820921f7c898d8cae0470d29296226a
Michael Borosh <mborosh@uchicago.edu>
2013-11-12 23:53:46 -0600

    minor cleanup of shm-boost.cpp

--- shm-boost.cpp
+++ shm-boost.cpp
@@ -12,24 +12,13 @@ using namespace boost::interprocess;
 
 //Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
 //This allocator will allow placing containers in the segment
-//typedef allocator<int, managed_shared_memory::segment_manager>  ShmemAllocator;
 typedef allocator<char, managed_shared_memory::segment_manager>  CharAllocator;
 
 //Alias a vector that uses the previous STL-like allocator so that allocates
 //its values from the segment
-//typedef vector<int, ShmemAllocator> MyVector;
 typedef basic_string<char, std::char_traits<char>, CharAllocator>  MyShmString;
 typedef allocator<MyShmString, managed_shared_memory::segment_manager>  StringAllocator;
 typedef vector<MyShmString, StringAllocator>  MyShmStringVector;
-
-/*
-basic_string serializetm(struct TradeMessage){
-basic_string mystring;
-sprintf(mystring,"%s","hey!");
-return mystring;
-}
-*/
-
 
 //Main function. For parent process argc == 1, for child process argc == 2
 int main(int argc, char *argv[])
@@ -45,26 +34,12 @@ int main(int argc, char *argv[])
       //Create a new segment with given name and size
       managed_shared_memory segment(create_only, "MySharedMemory", 65536);
 
-/*using boost::interprocess;
-shared_memory_object shm_obj
-   (open_only                    //only open
-   ,"shared_memory"              //name
-   ,read_write                   //read-write mode
-   );
-*/
 
       //Initialize shared memory STL-compatible allocator
-//      const ShmemAllocator alloc_inst (segment.get_segment_manager());
       const CharAllocator alloc_char (segment.get_segment_manager());
       const StringAllocator alloc_str (segment.get_segment_manager());
 
 	MyShmString mystring(alloc_char);
-	//mystring="somedata";
-	/*
-	char* mystr = (char*) malloc (50*sizeof(char));
-	sprintf(mystr,"%s","somedata");
-	mystring = mystr;
-	*/
 
 	// Convert Trade Message to String
 	//
@@ -77,20 +52,18 @@ shared_memory_object shm_obj
 	sprintf(mystr2,"%s,%s,%lu",mytm->symbol,mytm->price,mytm->quantity);
 	mystring=mystr2;
 	std::cout<<"Server pushes trade message stored as CSV string:"<<std::endl<<mystr2<<std::endl;
-	//end convert tm to str
+	//end convert trademessage to string
 
 
 	// Convert String to Trade Message
 	//
 	struct TradeMessage *mytm2 = (struct TradeMessage*) malloc (sizeof(TradeMessage));
 	sscanf(mystr2,"%[^,],%[^,],%lu",mytm2->symbol,mytm2->price,&(mytm2->quantity));
-//%[^,]
-
 //	printf("Trade Message Converted from CSV String:\n%s|",mytm2->symbol);
 //	printf("%s|",mytm2->price);
 //	printf("%lu\n",mytm2->quantity);
 	//mystring=(mytm2->symbol);		
-	
+	//end convert string to trade message
 
 
 	MyShmStringVector myvector(alloc_str);
@@ -99,22 +72,6 @@ shared_memory_object shm_obj
 	MyShmStringVector *myshmvector = segment.construct<MyShmStringVector>("myshmvector")(alloc_str);
 	myshmvector->insert(myshmvector->begin(), 10, mystring);
 
-      //Construct a vector named "MyVector" in shared memory with argument alloc_inst
-      //MyVector *myvector = segment.construct<MyVector>("MyVector")(alloc_inst);
-
-/*      for(int i = 0; i < 100; ++i)  //Insert data in the vector
-         myvector->push_back(i);
-*/
-	//myvector->push_back("somedata");
-	//myvector->push_back("somemoredata");
-/*	
-char* my_data = (char*) malloc (50*sizeof(char));
-	sprintf(my_data,"%s","somedata");
-	myvector->push_back(my_data);
-	char* my_data2 = (char*) malloc (50*sizeof(char));
-	sprintf(my_data2,"%s","somemoredata");
-	myvector->push_back(my_data2);
-*/
 
       //Launch child process
       std::string s(argv[0]); s += " child ";
@@ -122,7 +79,6 @@ char* my_data = (char*) malloc (50*sizeof(char));
          return 1;
 
       //Check child has destroyed the vector
-//      if(segment.find<MyVector>("MyVector").first)
       if(segment.find<MyShmStringVector>("myshmvector").first)
          return 1;
    }
@@ -133,15 +89,7 @@ char* my_data = (char*) malloc (50*sizeof(char));
       //Find the vector using the c-string name
       MyShmStringVector *myvector = segment.find<MyShmStringVector>("myshmvector").first;
 
-      //Use vector in reverse order
-//      std::sort(myvector->rbegin(), myvector->rend());
-//std::cout<<myvector->back();
 std::cout<<"Client Pulls Trade Message Stored As CSV String:"<<std::endl<<myvector->front()<<std::endl;
-//	std::cout<<(*myvector)[0];
-
-//	std::cout << myvector->rbegin();
-//for( std::vector<char*, ShmemAllocator>::const_iterator i = myvector->rbegin(); i != myvector->rend(); ++i)
-//    std::cout << *i << ' ';
 
       //When done, destroy the vector from the segment
       segment.destroy<MyShmStringVector>("myshmvector");
