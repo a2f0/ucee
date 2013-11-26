350d11be5c5c6e7ebce9dad87e8e23ea9ff2b073
Michael Borosh <mborosh@uchicago.edu>
2013-11-25 23:48:46 -0600

    reporting engine updates

--- db.h
+++ db.h
@@ -4,7 +4,6 @@
 #include "sqlite3.h"
 #include "messages.h"
 #include <list>
-#include <cstring>
 
 using namespace std;
 
@@ -12,3 +11,11 @@ int add_row(Order myorder);
 int del_row(Order myorder);
 int modify_row(Modify mymodify);
 list<Order> get_db(std::string dbname, std::string tablename);
+
+
+
+//DATABASE SCHEMAS:
+
+//1. OrderBook.db
+
+//2. Reporting.db
