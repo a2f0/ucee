e9274f085605623c71fb75b1a866d5c249db6c4e
Michael Borosh <mborosh@uchicago.edu>
2013-11-26 22:25:20 -0600

    semaphore example

--- shm_cl.cpp
+++ shm_cl.cpp
@@ -4,28 +4,70 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
+#include <cstring>
+#define SHM_KEY 9991
+#define SEM_KEY 9999991
 using namespace std;
 
+union semun {
+	int val;
+	struct semid_ds *buf;
+	ushort *array;
+};
+
+int wait_and_lock(int semset_id){
+
+        union semun sem_info;
+	struct sembuf actions[2];
+        actions[0].sem_num = 1;
+        actions[0].sem_flg = SEM_UNDO;
+        actions[0].sem_op = 0;
+        actions[1].sem_num = 0;
+        actions[1].sem_flg = SEM_UNDO;
+        actions[1].sem_op = +1;
+
+        if(semop(semset_id, actions, 2) == -1)
+                printf("ERROR:  semop---locking");
+return 0;
+}
+
+int release_lock(int semset_id){
+
+	union semun sem_info;
+        struct sembuf actions[1];
+        actions[0].sem_num=0;
+        actions[0].sem_flg=SEM_UNDO;
+        actions[0].sem_op=-1;
+
+        if(semop(semset_id,actions,1) == -1)
+                printf("ERROR:  semop---unlocking");
+return 0;
+}
+
+
 int main(){
-//char c;
-int shmid;
+int shm_id, semset_id;
 key_t mykey=5678;
 size_t mysize = 27;
-char *shm, *s;
+char *mem_ptr, *s;
 
-if( (shmid = shmget(mykey, mysize, 0666)) < 0)
+if( (shm_id = shmget(SHM_KEY, mysize, 0777|IPC_CREAT)) < 0)
 	cout << "Error: shmget" << endl;
 
-if ((shm = (char*) shmat(shmid, NULL, 0)) == (char*) -1) {
+if ((mem_ptr = (char*) shmat(shm_id, NULL, 0)) == (char*) -1) {
 	cout << "Error: shmat" << endl;
 }
 
-for (s=shm; *s != NULL; s++)
-	putchar(*s);
-putchar('\n');
 
-*shm = '*';
+semset_id = semget(SEM_KEY, 2, 0);
+wait_and_lock(semset_id);
+printf("Shared memory says:  %s",mem_ptr);
+release_lock(semset_id);
 
+
+
+shmdt(mem_ptr);
 return 0;
 
 }
--- shm_sv.cpp
+++ shm_sv.cpp
@@ -4,31 +4,91 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
+#include <cstring>
+#define SHM_KEY 9991
+#define SEM_KEY 9999991
 using namespace std;
+
+int shm_id, semset_id; //global for cleanup()
+union semun{
+int val;
+struct semid_ds *buf;
+ushort *array;
+};
+void cleanup(int); //cleanup for semaphores
+
+
+void cleanup(int n){
+        shmctl(shm_id, IPC_RMID, NULL);
+        semctl(semset_id, 0, IPC_RMID, NULL);
+}
+
+
+int set_sem_value(int semset_id, int semnum, int val){
+        union semun initval;
+        initval.val = val;
+        if (semctl(semset_id, semnum, SETVAL, initval) == -1)
+                printf("ERROR:  semctl");
+return 0;
+}
+
+int wait_and_lock(int semset_id){
+
+        struct sembuf actions[2];
+        actions[0].sem_num = 0;
+        actions[0].sem_flg = SEM_UNDO;
+        actions[0].sem_op = 0;
+        actions[1].sem_num = 1;
+        actions[1].sem_flg = SEM_UNDO;
+        actions[1].sem_op = +1;
+
+        if(semop(semset_id, actions, 2) == -1)
+                printf("ERROR:  semop---locking");
+return 0;
+}
+
+int release_lock(int semset_id){
+        struct sembuf actions[1];
+        actions[0].sem_num=1;
+        actions[0].sem_flg=SEM_UNDO;
+        actions[0].sem_op=-1;
+
+        if(semop(semset_id,actions,1) == -1)
+                printf("ERROR:  semop---unlocking");
+return 0;
+}
+
+
 
 int main(){
 char c;
-int shmid;
+int i;
 key_t mykey=5678;
 size_t mysize = 27;
-char *shm, *s;
+char *mem_ptr, *s;
 
-if( (shmid = shmget(mykey, mysize, IPC_CREAT | 0666)) < 0)
+
+if( (shm_id = shmget(SHM_KEY, mysize, IPC_CREAT | 0777)) < 0)
 	cout << "Error: shmget" << endl;
 
-if ((shm = (char*) shmat(shmid, NULL, 0)) == (char*) -1) {
+if ((mem_ptr = (char*) shmat(shm_id, NULL, 0)) == (char*) -1) {
 	cout << "Error: shmat" << endl;
 }
 
-s=shm;
+if ( (semset_id = semget (SEM_KEY, 2, (IPC_CREAT | 0777 | IPC_EXCL))) == -1)
+	printf("ERROR:  semget");
 
-for (c='a'; c <= 'z'; c++)
-	*s++ = c;
-*s=NULL;
+set_sem_value (semset_id, 0, 0);
+set_sem_value (semset_id, 1, 0);
 
-while (*shm != '*')
-	sleep(1);
-
-return 0;
-
+for(i=0; i<10; i++){
+wait_and_lock(semset_id);
+strcpy(mem_ptr, "WRITINGTOSHMTEXT");
+release_lock(semset_id);
 }
+
+//cleanup(0);
+return 0;
+}
+
