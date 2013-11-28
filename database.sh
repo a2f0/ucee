9e5ca7b7777c4835b859435d369b7d0ef08a8d1f
mborosh <mborosh@uchicago.edu>
2013-11-28 10:35:26 +0000

    database.sh edited online with Bitbucket

--- database.sh
+++ database.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sqlite3 OrderBook.db "create table t1 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
-sqlite3 Reporting.db "create table t1 (symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
+sqlite3 OrderBook.db "create table t2 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
+sqlite3 Reporting.db "create table t1 (symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT, orderid1 TEXT, orderid2 TEXT);"
