1d45d8b8a2ef3ba423980a1f397c36816034c2ec
Michael Borosh <mborosh@uchicago.edu>
2013-11-29 15:47:01 -0600

    rpt

--- rpteng.cpp
+++ rpteng.cpp
@@ -95,11 +95,12 @@ mykey = ftok(METOREKEY1,'b');
 //size_t mysize = 27;
 size_t mysize = sizeof(struct ReportingMessage);
 struct ReportingMessage* rm = (struct ReportingMessage*) malloc (sizeof(ReportingMessage));
+struct ReportingMessage* rm = (struct ReportingMessage*) shmat(shmid, NULL, 0);
 if( (shmid = shmget(mykey, mysize, 0666 | IPC_CREAT)) < 0)
         cout << "Error: shmget" << endl;
-if ((rm = (struct ReportingMessage*) shmat(shmid, NULL, 0)) == (struct ReportingMessage*) -1) {
-        cout << "Error: shmat" << endl;
-}
+//if ((rm = (struct ReportingMessage*) shmat(shmid, NULL, 0)) == (struct ReportingMessage*) -1) {
+//        cout << "Error: shmat" << endl;
+//}
 
 
 //
