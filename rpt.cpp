14dd8712a51e8a08d09a7bfe47cdf3d0fa6b99e9
Michael Borosh <mborosh@uchicago.edu>
2013-11-29 03:17:02 -0600

    reporting update

--- rpt.cpp
+++ rpt.cpp
@@ -110,22 +110,22 @@ return 0;
 int summary_rpt(){
 printf("\n\nSUMMARY REPORT:\n\n");
 int rc,c;
-char* query1 = (char*) malloc (1024*sizeof(char));
-char* query2 = (char*) malloc (1024*sizeof(char));
+//char* query1 = (char*) malloc (1024*sizeof(char));
+//char* query2 = (char*) malloc (1024*sizeof(char));
 char* query3 = (char*) malloc (1024*sizeof(char));
 char* query4 = (char*) malloc (1024*sizeof(char));
 char* query5 = (char*) malloc (1024*sizeof(char));
 char* query6 = (char*) malloc (1024*sizeof(char));
 char* query7 = (char*) malloc (1024*sizeof(char));
-sprintf(query1,"%s","ATTACH 'OrderBook.db' as odb;");
-sprintf(query2,"%s","ATTACH 'Reporting.db' as rdb;");
-sprintf(query3,"%s","SELECT COUNT(*) FROM odb.t2;");
-sprintf(query4,"%s","SELECT COUNT(*) FROM rdb.t1;");
-sprintf(query5,"%s","SELECT SUM(quantity) FROM odb.t2 WHERE (side='0');");
-sprintf(query6,"%s","SELECT SUM(quantity) FROM odb.t2 WHERE (side='1');");
-sprintf(query7,"%s","SELECT SUM(quantity) FROM odb.t2;");
-sqlite3_stmt *stmt1;
-sqlite3_stmt *stmt2;
+//sprintf(query1,"%s","ATTACH 'OrderBook.db' as odb;");
+//sprintf(query2,"%s","ATTACH 'Reporting.db' as rdb;");
+sprintf(query3,"%s","SELECT COUNT(*) FROM t2;");
+sprintf(query4,"%s","SELECT COUNT(*) FROM t3;");
+sprintf(query5,"%s","SELECT SUM(quantity) FROM t2 WHERE (side='0');");
+sprintf(query6,"%s","SELECT SUM(quantity) FROM t2 WHERE (side='1');");
+sprintf(query7,"%s","SELECT SUM(quantity) FROM t2;");
+//sqlite3_stmt *stmt1;
+//sqlite3_stmt *stmt2;
 sqlite3_stmt *stmt3;
 sqlite3_stmt *stmt4;
 sqlite3_stmt *stmt5;
@@ -133,76 +133,76 @@ sqlite3_stmt *stmt6;
 sqlite3_stmt *stmt7;
 sqlite3* mydb = create_db();
 sqlite3_open("OrderBook.db",&mydb);
-if ( (rc = sqlite3_prepare_v2(mydb, query1,-1, &stmt1, NULL )) != SQLITE_OK)
-        cout << sqlite3_errmsg(mydb);
+//if ( (rc = sqlite3_prepare_v2(mydb, query1,-1, &stmt1, NULL )) != SQLITE_OK)
+//        cout << sqlite3_errmsg(mydb);
 //while ( (c=sqlite3_step(stmt1)) == 100 ){
 //; 
 //}
-sqlite3_finalize(stmt1);
+//sqlite3_finalize(stmt1);
 
 
 
-if ( (rc = sqlite3_prepare_v2(mydb, query2,-1, &stmt2, NULL )) != SQLITE_OK)
-        cout << sqlite3_errmsg(mydb);
+//if ( (rc = sqlite3_prepare_v2(mydb, query2,-1, &stmt2, NULL )) != SQLITE_OK)
+//        cout << sqlite3_errmsg(mydb);
 //while ( (c=sqlite3_step(stmt1)) == 100 ){
 //; 
 //}
-sqlite3_finalize(stmt2);
+//sqlite3_finalize(stmt2);
 
-printf("%s\n: ","Total Number of Orders");
+printf("%s: ","Total Number of Orders");
 if ( (rc = sqlite3_prepare_v2(mydb, query3,-1, &stmt3, NULL )) != SQLITE_OK)
         cout << sqlite3_errmsg(mydb);
 while ( (c=sqlite3_step(stmt3)) == 100 ){
                 printf("%s \n ",(char*)sqlite3_column_text(stmt3,0));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt3,1));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt3,2));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt3,1));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt3,2));
 //; 
 }
 sqlite3_finalize(stmt3);
 
-printf("%s\n: ","Total Number of Trades");
+printf("%s: ","Total Number of Trades");
 if ( (rc = sqlite3_prepare_v2(mydb, query4,-1, &stmt4, NULL )) != SQLITE_OK)
         cout << sqlite3_errmsg(mydb);
 while ( (c=sqlite3_step(stmt4)) == 100 ){
                 printf("%s \n ",(char*)sqlite3_column_text(stmt4,0));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt4,1));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt4,2));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt4,1));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt4,2));
 //; 
 }
 sqlite3_finalize(stmt4);
 
-printf("%s\n: ","Side 0 Volume");
+printf("%s: ","Side 0 Volume");
 if ( (rc = sqlite3_prepare_v2(mydb, query5,-1, &stmt5, NULL )) != SQLITE_OK)
         cout << sqlite3_errmsg(mydb);
 while ( (c=sqlite3_step(stmt5)) == 100 ){
                 printf("%s \n ",(char*)sqlite3_column_text(stmt5,0));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt5,1));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt5,2));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt5,1));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt5,2));
 //; 
 }
 sqlite3_finalize(stmt5);
 
 
-printf("%s\n: ","Side 1 Volume");
+printf("%s: ","Side 1 Volume");
 if ( (rc = sqlite3_prepare_v2(mydb, query6,-1, &stmt6, NULL )) != SQLITE_OK)
         cout << sqlite3_errmsg(mydb);
 while ( (c=sqlite3_step(stmt6)) == 100 ){
 //;
                 printf("%s \n ",(char*)sqlite3_column_text(stmt6,0));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt6,1));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt6,2));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt6,1));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt6,2));
 }
 sqlite3_finalize(stmt6);
 
 
-printf("%s\n: ","Total Volume");
+printf("%s: ","Total Volume");
 if ( (rc = sqlite3_prepare_v2(mydb, query7,-1, &stmt7, NULL )) != SQLITE_OK)
         cout << sqlite3_errmsg(mydb);
 while ( (c=sqlite3_step(stmt7)) == 100 ){	
 //      for(int j=0; j<6; j++) //number of columns in the database as defined in database.sh
                 printf("%s \n ",(char*)sqlite3_column_text(stmt7,0));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt7,1));
-                printf("%s \n ",(char*)sqlite3_column_text(stmt7,2));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt7,1));
+//                printf("%s \n ",(char*)sqlite3_column_text(stmt7,2));
 //      printf("\n");
 }
 
