0b15d342e10a6447f8c2e22fc5a308038bd24086
Michael Borosh <mborosh@uchicago.edu>
2013-11-29 19:14:08 -0600

    change schema s.th. price is text not double

--- database.sh
+++ database.sh
@@ -6,9 +6,11 @@
 # Third  Table (t3): Records All Trades (trades are never removed)
 #
 sqlite3 OrderBook.db "drop table if exists t1;"
-sqlite3 OrderBook.db "create table t1 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
+# sqlite3 OrderBook.db "create table t1 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
+sqlite3 OrderBook.db "create table t1 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price TEXT, quantity UNSIGNED BIG INT);"
 sqlite3 OrderBook.db "drop table if exists t2;"
-sqlite3 OrderBook.db "create table t2 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
+sqlite3 OrderBook.db "create table t2 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price TEXT, quantity UNSIGNED BIG INT);"
+#sqlite3 OrderBook.db "create table t2 (t1key TEXT PRIMARY KEY, account TEXT, user TEXT, ordertype INTEGER, timestamp UNSIGNED BIG INT, side INTEGER, symbol TEXT, price DOUBLE, quantity UNSIGNED BIG INT);"
 sqlite3 OrderBook.db "drop table if exists t3;"
 sqlite3 OrderBook.db "create table t3 (symbol TEXT, timestamp UNSIGNED BIG INT, price DOUBLE, quantity UNSIGNED BIG INT, orderid1 TEXT, orderid2 TEXT);"
 sqlite3 OrderBook.db < triggers.sql
