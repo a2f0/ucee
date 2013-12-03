2424ea806c1872df82c43cebd7557eacaad6cd20
Marcelo Julio Alvisio <alvisio@linux1.cs.uchicago.edu>
2013-12-03 13:19:34 -0600

    new keys

--- keys.h
+++ keys.h
@@ -1,15 +1,15 @@
 #ifndef KEYS_H
 #define KEYS_H
 
-#define CMTOMEKEY1 "/etc/uucp"
+#define CMTOMEKEY1 "/etc/fuse.conf"
 #define METOCMKEY1 "/etc/usb_modeswitch.conf"
-#define METOTPKEY1 "/etc/sensors3.conf"
+#define METOTPKEY1 "/etc/fstab.d"
 #define CMTOBPKEY1 "/etc/smartd.conf"
 #define METOREKEY1 "/etc/gai.conf"
 #define SEMKEY1 "/etc/locale.gen"
 #define SEMKEY2 "/etc/gssapi_mech.conf"
 #define SEMKEY3 "/etc/golang"
-#define SEMDB "/etc/xodim.conf"
+#define SEMDB "/etc/nail.rc"
 #define METORESEM "/etc/groovy"
 #define MULTICAST_ADDRESS "239.192.07.07"
 #define MULTICAST_PORT "1234"
